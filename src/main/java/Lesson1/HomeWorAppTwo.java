3eba99a5fc716af8375e9a68494d5ab10c323169
Aleksei955 <mr.alex.chernikov95@mail.ru>
2022-10-25 14:16:42 +0400

    Готовое ДЗ

--- src/main/java/Lesson1/HomeWorAppTwo.java
+++ src/main/java/Lesson1/HomeWorAppTwo.java
@@ -3,18 +3,18 @@ package Lesson1;
 public class HomeWorAppTwo {
     public static void main(String[] args){
 
-/*        System.out.println(within10and20(5, 10));*/
-/*        isPositiveOrNegative(0);*/
-/*        System.out.println(positiveAndNegative(0));*/
-          stringAndNumber("???", 20);
+       System.out.println(within10and20(5, 10));
+       isPositiveOrNegative(0);
+       System.out.println(positiveAndNegative(0));
+       stringAndNumber("???", 20);
 
     }
-/*    public static boolean within10and20 (int x1 , int x2) {
+   public static boolean within10and20 (int x1 , int x2) {
 
         if (x1 + x2 > 10 && x1 + x2 < 20) return true;
         return false;
-    }*/
-/*    public static void isPositiveOrNegative (int x){
+    }
+    public static void isPositiveOrNegative (int x){
         if (x >= 0){
             System.out.println("Positive");
         } else {
@@ -22,11 +22,11 @@ public class HomeWorAppTwo {
 
         }
 
-    }*/
-/*    public static boolean positiveAndNegative(int x) {
+    }
+    public static boolean positiveAndNegative(int x) {
         if (x <= 0) return true;
         return false;
-    }*/
+    }
     public static void stringAndNumber(String word, int times){
         for (int i = 2; i < 4; i++) {
             System.out.println("Great" + word);
